fe09270a41def81b959796ac91a2e6d74b4713c7
Diego Caponera <diego.caponera@gmail.com>
2012-06-18 19:02:17 +0200

    BASE_PATH auto configured now.

--- config.php
+++ config.php
@@ -4,14 +4,25 @@
 define("SRC_PATH", __DIR__."/src/");
 define('DEBUG', true);
 define("SESSION_NAME", "mw");
-$packages = array("MWCore", "Backstage", "App");
+
+// Packages to load
+$packages = array(
+	"MWCore",
+	"Backstage",
+	"Rest",
+	"App"
+);
+
+// Solves a PHP 5.x warning
 date_default_timezone_set("Europe/Berlin");
+
+// Sets error reporting level
 error_reporting(0);
 
 // Paths and Site Config
-define("DOMAIN",		"http://localhost/");	
-define("BASE_PATH",		DOMAIN."_projects/mw/");
-define("ASSET_PATH",	DOMAIN."_projects/mw/web/");
+define("DOMAIN",		'http://' . $_SERVER['SERVER_NAME']);
+define("BASE_PATH",		DOMAIN . str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+define("ASSET_PATH",	BASE_PATH . "web/");
 
 // Server Config
 define('REWRITE_RULE',	'^(.*)$ index.php');
